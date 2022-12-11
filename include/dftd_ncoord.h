0921efeaaa1618ec503dca1d981516dbea18e390
Marvin Friede <51965259+marvinfriede@users.noreply.github.com>
2022-12-11 09:49:12 +0100

    Bugfix and update (#6)
    - fixes some bugs in the code
    - update parameters from Fortran's reference.inc
      - fixes wrong number of reference systems in `refn`
      - fixes `refc` for some elements
    - fixes wrong charges (`clsq`) and charge scaling (`clsh`) for elements added in the periodic extension
    - add parameters for new functionals

--- include/dftd_ncoord.h
+++ include/dftd_ncoord.h
@@ -27,27 +27,86 @@
 #include "dftd_matrix.h"
 
 namespace dftd {
-// Calculate all distance pairs and store in matrix
+
+/**
+ * Calculate all distance pairs and store in matrix
+ * 
+ * @param mol Molecule object.
+ * @param dist Distance matrix (inout).
+ * @return Exit status.
+*/
 extern int calc_distances(TMolecule& mol, TMatrix<double>& dist);
 
-// Calculate error function coordination number
+/**
+ * Calculate error function coordination number.
+ * 
+ * @param mol Molecule object.
+ * @param dist Distance matrix.
+ * @param cn Vector of coordination numbers.
+ * @param thr Real-space cutoff (default: 1600.0).
+ * @return Exit status.
+*/
 extern int ncoord_erf(TMolecule& mol, TMatrix<double>& dist,
                       TVector<double>& cn, double thr = 1600.0);
 
-// Calculate covalent coordination number
+/**
+ * Calculate covalent coordination number for DFT-D4.
+ * 
+ * @param mol Molecule object.
+ * @param dist Distance matrix.
+ * @param cn Vector of coordination numbers.
+ * @param thr Real-space cutoff (default: 1600.0).
+ * @return Exit status.
+*/
 extern int ncoord_d4(TMolecule& mol, TMatrix<double>& dist,
                      TVector<double>& cn, double thr = 1600.0);
 
-// Calculate error function coordination number and derivative
-// w.r.t. nuclear coordinates
+/**
+ * Calculate error function coordination number and derivative
+ * w.r.t. nuclear coordinates.
+ * 
+ * @param mol Molecule object.
+ * @param dist Distance matrix.
+ * @param cn Vector of coordination numbers.
+ * @param dcndr Derivative of coordination number.
+ * @param thr Real-space cutoff (default: 1600.0).
+ * @return Exit status.
+*/
 extern int dncoord_erf(TMolecule& mol, TMatrix<double>& dist,
                        TVector<double>& cn, TMatrix<double>& dcndr,
                        double thr = 1600.0);
 
-// Calculate covalent coordination number and derivative
-// w.r.t. nuclear coordinates
+/**
+ * Calculate covalent coordination number and derivative
+ * w.r.t. nuclear coordinates
+ * 
+ * @param mol Molecule object.
+ * @param dist Distance matrix.
+ * @param cn Vector of coordination numbers.
+ * @param dcndr Derivative of coordination number.
+ * @param thr Real-space cutoff (default: 1600.0).
+ * @return Exit status.
+*/
 extern int dncoord_d4(TMolecule& mol, TMatrix<double>& dist,
                       TVector<double>& cn, TMatrix<double>& dcndr,
                       double thr = 1600.0);
+
+/**
+ * Error function counting function for coordination number contributions.
+ *
+ * @param k Steepness of the counting function.
+ * @param rr Current distance over cutoff radius.
+ * @return Value of the counting function.
+ */
+extern double erf_count(double k, double rr);
+
+/**
+ * Derivative of the counting function w.r.t. the distance.
+ * 
+ * @param k Steepness of the counting function.
+ * @param rr Cutoff radius.
+ * @return Derivative of the counting function.
+ */
+extern double derf_count(double k, double rr);
 
 };  // namespace dftd
--- src/dftd_ncoord.cpp
+++ src/dftd_ncoord.cpp
@@ -34,13 +34,18 @@
 namespace dftd {
 
 // convert bohr (a.u.) to Ångström and back
-static const double autoaa = 0.52917726;
+static const double autoaa = 0.52917721090449243;
 static const double aatoau = 1.0 / autoaa;
 
-// covalent radii (taken from Pyykko and Atsumi, Chem. Eur. J. 15, 2009,
-// 188-197), values for metals decreased by 10 %
-static const double rad[119]{
-    0.0,  0.32, 0.46,                                            //  H,He
+/**
+ * Covalent radii (taken from Pyykko and Atsumi, Chem. Eur. J. 15, 2009,
+ * 188-197), values for metals decreased by 10 %.
+ * 
+ * These values are actually never used in the code.
+ * Only the scaled values below are used (`rad`).
+*/
+static const double covalent_rad_d3[119]{ 0.0,
+    0.32, 0.46,                                                  //  H,He
     1.20, 0.94, 0.77, 0.75, 0.71, 0.63, 0.64, 0.67,              //  Li-Ne
     1.40, 1.25, 1.13, 1.04, 1.10, 1.02, 0.99, 0.96,              //  Na-Ar
     1.76, 1.54,                                                  //  K,Ca
@@ -59,6 +64,53 @@ static const double rad[119]{
     1.49, 1.51, 1.51, 1.48, 1.50, 1.56, 1.58,                    //  Cm-No
     1.45, 1.41, 1.34, 1.29, 1.27, 1.21, 1.16, 1.15, 1.09, 1.22,  //  Lr-Cn
     1.22, 1.29, 1.46, 1.58, 1.48, 1.41                           //  Nh-Og
+};
+
+/**
+ * D3 covalent radii used to construct the coordination number.
+ * rad = covalent_rad_d3 * 4.0/3.0 * aatoau
+ */
+static const double rad[119]{ 0.0,
+    0.8062831465047213,     1.1590320231005369,     3.0235617993927044,
+    2.3684567428576182,     1.9401188212769855,     1.8897261246204402,
+    1.7889407313073502,     1.5873699446811698,     1.6125662930094427,
+    1.6881553379942602,     3.5274887659581555,     3.1495435410340673,
+    2.8471873610947966,     2.6204202261403440,     2.7715983161099791,
+    2.5700275294837986,     2.4944384844989811,     2.4188494395141635,
+    4.4345573057759662,     3.8802376425539711,     3.3511143276602473,
+    3.0739544960492493,     3.0487581477209771,     2.7715983161099791,
+    2.6960092711251620,     2.6204202261403440,     2.5196348328272538,
+    2.4944384844989811,     2.5448311811555264,     2.7464019677817069,
+    2.8219910127665244,     2.7464019677817069,     2.8975800577513415,
+    2.7715983161099791,     2.8723837094230693,     2.9479727544078864,
+    4.7621098340435095,     4.2077901708215135,     3.7038632042560633,
+    3.5022924176298824,     3.3259179793319751,     3.1243471927057946,
+    2.8975800577513415,     2.8471873610947966,     2.8471873610947966,
+    2.7212056194534342,     2.8975800577513415,     3.0991508443775224,
+    3.2251325860188853,     3.1747398893623395,     3.1747398893623395,
+    3.0991508443775224,     3.3259179793319751,     3.3007216310037024,
+    5.2660368006089602,     4.4345573057759662,     4.0818084291801515,
+    3.7038632042560633,     3.9810230358670613,     3.9558266875387886,
+    3.9306303392105164,     3.9054339908822433,     3.8046485975691535,
+    3.8298449458974257,     3.8046485975691535,     3.7794522492408804,
+    3.7542559009126082,     3.7542559009126082,     3.7290595525843355,
+    3.8550412942256984,     3.6786668559277906,     3.4518997209733380,
+    3.3007216310037024,     3.0991508443775224,     2.9731691027361595,
+    2.9227764060796142,     2.7967946644382522,     2.8219910127665244,
+    2.8471873610947966,     3.3259179793319751,     3.2755252826754302,
+    3.2755252826754302,     3.4267033726450653,     3.3007216310037024,
+    3.4770960693016097,     3.5778814626147004,     5.0644660139827797,
+    4.5605390474173291,     4.2077901708215135,     3.9810230358670613,
+    3.8298449458974257,     3.8550412942256984,     3.8802376425539711,
+    3.9054339908822433,     3.7542559009126082,     3.7542559009126082,
+    3.8046485975691535,     3.8046485975691535,     3.7290595525843355,
+    3.7794522492408804,     3.9306303392105164,     3.9810230358670613,
+    3.6534705075995175,     3.5526851142864277,     3.3763106759885204,
+    3.2503289343471575,     3.1999362376906122,     3.0487581477209771,
+    2.9227764060796142,     2.8975800577513415,     2.7464019677817069,
+    3.0739544960492493,     3.4267033726450653,     3.6030778109429726,
+    3.6786668559277906,     3.9810230358670613,     3.7290595525843355,
+    3.9558266875387886 
 };
 
 // pauling EN's
@@ -90,10 +142,9 @@ static const double k4 = 4.10451;
 static const double k5 = 19.08857;
 static const double k6 = 2 * pow(11.28174, 2);
 static const double hlfosqrtpi = 1.0 / 1.77245385091;
+static const double cn_max = 8.0;
 
-/*
- * Calculate all distance pairs and store in matrix
- */
+
 int calc_distances(TMolecule& mol, TMatrix<double>& dist) {
   double rx = 0.0, ry = 0.0, rz = 0.0, tmp = 0.0;
   for (int i = 0; i != mol.NAtoms; i++) {
@@ -111,104 +162,100 @@ int calc_distances(TMolecule& mol, TMatrix<double>& dist) {
   return EXIT_SUCCESS;
 }
 
-/*
- * Calculate covalent coordination number
- */
+
 int ncoord_d4(TMolecule& mol, TMatrix<double>& dist, TVector<double>& cn,
               double thr) {
   double r = 0.0, rcovij = 0.0, rr = 0.0;
   double den = 0.0;
-  double tmp = 0.0;
+  double countf = 0.0;
+  int izp, jzp;
 
   for (int i = 0; i != mol.NAtoms; i++) {
+    izp = mol.at(i);
     for (int j = 0; j != i; j++) {
+      jzp = mol.at(i);
+
       r = dist(i, j);
-      rcovij = rad[mol.at(i)] + rad[mol.at(j)];
-      rr = 3.0 * r / (4.0 * rcovij * aatoau);
-      den =
-          k4 *
-          std::exp(-pow((abs(en[mol.at(i)] - en[mol.at(j)]) + k5), 2) / k6);
-      tmp = den * 0.5 * (1.0 + std::erf(-kn * (rr - 1.0)));
-      cn(i) += tmp;
-      cn(j) += tmp;
+      rcovij = rad[izp] + rad[jzp];
+      rr = r / rcovij;
+      den = k4 * std::exp(-pow((abs(en[izp] - en[jzp]) + k5), 2) / k6);
+      countf = den * erf_count(kn, rr);
+      
+      cn(i) += countf;
+      cn(j) += countf;
     }
   }
   return EXIT_SUCCESS;
 }
 
-/*
- * Calculate covalent coordination number and derivative w.r.t. nuclear
- * coordinates
- */
+
 int dncoord_d4(TMolecule& mol, TMatrix<double>& dist, TVector<double>& cn,
                TMatrix<double>& dcndr, double thr) {
   double r = 0.0, rcovij = 0.0, rr = 0.0;
   double rx = 0.0, ry = 0.0, rz = 0.0;
-  double tmp = 0.0, dtmp = 0.0, den = 0.0;
+  double countf = 0.0, dcountf = 0.0, den = 0.0;
+  int izp, jzp;
 
   for (int i = 0; i != mol.NAtoms; i++) {
+    izp = mol.at(i);
     for (int j = 0; j != i; j++) {
+      jzp = mol.at(j);
+
       r = dist(i, j);
       rx = (mol.xyz(j, 0) - mol.xyz(i, 0)) / r;
       ry = (mol.xyz(j, 1) - mol.xyz(i, 1)) / r;
       rz = (mol.xyz(j, 2) - mol.xyz(i, 2)) / r;
-      rcovij = 4.0 / 3.0 * aatoau * (rad[mol.at(i)] + rad[mol.at(j)]);
+
+      rcovij = rad[izp] + rad[jzp];
       rr = r / rcovij;
-      den =
-          k4 *
-          std::exp(-pow((abs(en[mol.at(i)] - en[mol.at(j)]) + k5), 2) / k6);
-      tmp = den * 0.5 * (1.0 + std::erf(-kn * (rr - 1.0)));
-      cn(i) += tmp;
-      cn(j) += tmp;
-      dtmp =
-          -den * hlfosqrtpi * kn * std::exp(-pow(kn * (rr - 1.0), 2)) / rcovij;
-      dcndr(j, 3 * j) += dtmp * rx;
-      dcndr(j, 3 * j + 1) += dtmp * ry;
-      dcndr(j, 3 * j + 2) += dtmp * rz;
-      dcndr(j, 3 * i) = dtmp * rx;
-      dcndr(j, 3 * i + 1) = dtmp * ry;
-      dcndr(j, 3 * i + 2) = dtmp * rz;
-      dcndr(i, 3 * j) = -dtmp * rx;
-      dcndr(i, 3 * j + 1) = -dtmp * ry;
-      dcndr(i, 3 * j + 2) = -dtmp * rz;
-      dcndr(i, 3 * i) += -dtmp * rx;
-      dcndr(i, 3 * i + 1) += -dtmp * ry;
-      dcndr(i, 3 * i + 2) += -dtmp * rz;
+      den = k4 * std::exp(-pow((fabs(en[izp] - en[jzp]) + k5), 2) / k6);
+      countf = den * erf_count(kn, rr);
+      cn(i) += countf;
+      cn(j) += countf;
+
+      dcountf = den * derf_count(kn, rr) / rcovij;
+      dcndr(j, 3 * j) += dcountf * rx;
+      dcndr(j, 3 * j + 1) += dcountf * ry;
+      dcndr(j, 3 * j + 2) += dcountf * rz;
+      dcndr(j, 3 * i) = dcountf * rx;
+      dcndr(j, 3 * i + 1) = dcountf * ry;
+      dcndr(j, 3 * i + 2) = dcountf * rz;
+      dcndr(i, 3 * j) = -dcountf * rx;
+      dcndr(i, 3 * j + 1) = -dcountf * ry;
+      dcndr(i, 3 * j + 2) = -dcountf * rz;
+      dcndr(i, 3 * i) += -dcountf * rx;
+      dcndr(i, 3 * i + 1) += -dcountf * ry;
+      dcndr(i, 3 * i + 2) += -dcountf * rz;
     }
   }
   return EXIT_SUCCESS;
 }
 
-/*
- * Coordination number with error function based counting
- */
+
 int ncoord_erf(TMolecule& mol, TMatrix<double>& dist, TVector<double>& cn,
                double thr) {
   double r = 0.0, rcovij = 0.0, rr = 0.0;
-  double tmp = 0.0;
+  double countf = 0.0;
 
   for (int i = 0; i != mol.NAtoms; i++) {
     for (int j = 0; j != i; j++) {
       r = dist(i, j);
       rcovij = rad[mol.at(i)] + rad[mol.at(j)];
-      rr = 3.0 * r / (4.0 * rcovij * aatoau);
-      tmp = 0.5 * (1.0 + std::erf(-kn * (rr - 1.0)));
-      cn(i) += tmp;
-      cn(j) += tmp;
+      rr = r / rcovij;
+      countf = erf_count(kn, rr);
+      cn(i) += countf;
+      cn(j) += countf;
     }
   }
   return EXIT_SUCCESS;
 }
 
-/*
- * Calculate error function coordination number and derivative w.r.t. nuclear
- * coordinates
- */
+
 int dncoord_erf(TMolecule& mol, TMatrix<double>& dist, TVector<double>& cn,
                 TMatrix<double>& dcndr, double thr) {
   double r = 0.0, rcovij = 0.0, rr = 0.0;
   double rx = 0.0, ry = 0.0, rz = 0.0;
-  double tmp = 0.0, dtmp = 0.0;
+  double countf = 0.0, dcountf = 0.0;
 
   for (int i = 0; i != mol.NAtoms; i++) {
     for (int j = 0; j != i; j++) {
@@ -216,27 +263,46 @@ int dncoord_erf(TMolecule& mol, TMatrix<double>& dist, TVector<double>& cn,
       rx = (mol.xyz(j, 0) - mol.xyz(i, 0)) / r;
       ry = (mol.xyz(j, 1) - mol.xyz(i, 1)) / r;
       rz = (mol.xyz(j, 2) - mol.xyz(i, 2)) / r;
-      rcovij = 4.0 / 3.0 * aatoau * (rad[mol.at(i)] + rad[mol.at(j)]);
+
+      rcovij = rad[mol.at(i)] + rad[mol.at(j)];
       rr = r / rcovij;
-      tmp = 0.5 * (1.0 + erf(-kn * (rr - 1.0)));
-      cn(i) += tmp;
-      cn(j) += tmp;
-      dtmp = -hlfosqrtpi * kn * std::exp(-pow(kn * (rr - 1.0), 2)) / rcovij;
-      dcndr(j, 3 * j) += dtmp * rx;
-      dcndr(j, 3 * j + 1) += dtmp * ry;
-      dcndr(j, 3 * j + 2) += dtmp * rz;
-      dcndr(j, 3 * i) = dtmp * rx;
-      dcndr(j, 3 * i + 1) = dtmp * ry;
-      dcndr(j, 3 * i + 2) = dtmp * rz;
-      dcndr(i, 3 * j) = -dtmp * rx;
-      dcndr(i, 3 * j + 1) = -dtmp * ry;
-      dcndr(i, 3 * j + 2) = -dtmp * rz;
-      dcndr(i, 3 * i) += -dtmp * rx;
-      dcndr(i, 3 * i + 1) += -dtmp * ry;
-      dcndr(i, 3 * i + 2) += -dtmp * rz;
+
+      countf = erf_count(kn, rr);
+      cn(i) += countf;
+      cn(j) += countf;
+
+      dcountf = derf_count(kn, rr) / rcovij;
+      dcndr(j, 3 * j) += dcountf * rx;
+      dcndr(j, 3 * j + 1) += dcountf * ry;
+      dcndr(j, 3 * j + 2) += dcountf * rz;
+      dcndr(j, 3 * i) = dcountf * rx;
+      dcndr(j, 3 * i + 1) = dcountf * ry;
+      dcndr(j, 3 * i + 2) = dcountf * rz;
+      dcndr(i, 3 * j) = -dcountf * rx;
+      dcndr(i, 3 * j + 1) = -dcountf * ry;
+      dcndr(i, 3 * j + 2) = -dcountf * rz;
+      dcndr(i, 3 * i) += -dcountf * rx;
+      dcndr(i, 3 * i + 1) += -dcountf * ry;
+      dcndr(i, 3 * i + 2) += -dcountf * rz;
     }
   }
+
+  // Cutoff function for large coordination numbers
+  for (int i = 0; i != mol.NAtoms; i++) {
+    cn(i) = log(1.0 + exp(cn_max)) - log(1.0 + exp(cn_max - cn(i)));
+  }
+
   return EXIT_SUCCESS;
+}
+
+
+double erf_count(double k, double rr) {
+  return 0.5 * (1.0 + std::erf(-k * (rr - 1.0)));
+}
+
+
+double derf_count(double k, double rr) {
+  return -hlfosqrtpi * k * std::exp(-pow(k * (rr - 1.0), 2));
 }
 
 }  // namespace dftd
