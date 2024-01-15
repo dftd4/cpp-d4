d4e535809eaed66842e5a7fbb18c12bbed8166c8
Marvin Friede <51965259+marvinfriede@users.noreply.github.com>
2024-01-15 14:22:59 +0100

    Fix reference CN for Ac and Cm (#29)

--- test/test_disp2.cpp
+++ test/test_disp2.cpp
@@ -84,7 +84,7 @@ int test_disp2() {
     actinides_atoms,
     actinides_coord,
     actinides_charge,
-    -2.761482717592804E-01
+    -2.760462533236626E-01
   );
   if (!info == EXIT_SUCCESS) return info;
 
